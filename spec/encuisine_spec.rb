236fabf2b0382e19995ce850cba402d5bdce2927
Cyril Blaecke <cyril@dilolabs.fr>
2022-04-08 17:30:18 +0200

    bundle gem encuisine

---
+++ spec/encuisine_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Encuisine do
+  it "has a version number" do
+    expect(Encuisine::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
